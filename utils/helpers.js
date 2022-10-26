006feee27d46d110da1234ae13181fdd79e919a8
OvernightSolo <sterlingtx75@gmail.com>
2022-10-26 01:55:20 -0500

    added schema, auth, and helpers

---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+function formatData(date) {
+  return `${new Date(date).getMonth() + 1}/${new Date(
+    date
+  ).getDate()}/${new Date(date).getFullYear()}`;
+}
+
+module.exports = { formatDate };
